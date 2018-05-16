27288cd8cda3014992b12ad6474a9300e538d019
Payam Esfandiari <me76@njit.edu>
2018-05-16 18:45:34 -0400

    elasticbean config added TODO: Aggregations for dashboard

--- crowdcur_server/settings.py
+++ crowdcur_server/settings.py
@@ -87,21 +87,31 @@ WSGI_APPLICATION = 'crowdcur_server.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
-DATABASES = {
-# 'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': '/Users/Payam/PycharmProjects/crowdcur_server/db.sqlite3',
-#     }
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'crowdcur_server',
-        'USER': 'Payam',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    },
-}
-
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
+else:
+    DATABASES = {
+        # 'default': {
+        #         'ENGINE': 'django.db.backends.sqlite3',
+        #         'NAME': '/Users/Payam/PycharmProjects/crowdcur_server/db.sqlite3',
+        #     }
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'crowdcur_server',
+            'USER': 'Payam',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
+        },
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
