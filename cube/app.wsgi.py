ac598a82584dff3f9db76744f0a3f33a23880877
Payam Esfandiari <me76@njit.edu>
2018-06-06 16:34:22 -0400

    Fixed cube server

--- cube/app.wsgi.py
+++ cube/app.wsgi.py
@@ -1,9 +1,9 @@
 import os.path
-from cubes.server import create_server
+from cubes.server import create_server,read_slicer_config
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Set the configuration file name (and possibly whole path) here
 CONFIG_PATH = os.path.join(CURRENT_DIR, "slicer3.ini")
-
-application = create_server(CONFIG_PATH)
+config = read_slicer_config(CONFIG_PATH)
+application = create_server(config)
